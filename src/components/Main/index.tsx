938f4aa5490b30d6d9aa9b36b66b3d0f368f4608
Evaldo Santos <jesantosjr@protonmail.com>
2021-03-27 20:51:41 -0300

    style: add styles to <Main /> component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,13 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Boilerplate</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="React Boilerplate Logo" />
+    <S.Title>React Boilerplate</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem;
+`
+
+export const Logo = styled.img`
+  width: 25rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  padding: 2rem;
+`
+
+export const Description = styled.h2``
