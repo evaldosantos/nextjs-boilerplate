34cf82159700df1cdae6e69a56d4d80fa639f180
Evaldo Santos <jesantosjr@protonmail.com>
2021-03-27 16:43:18 -0300

    chore: setup relative paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from '../styles/global'
+import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
