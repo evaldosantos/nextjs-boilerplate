169fd5c2b5236e48e10cd15ae23cd88dff33a295
Evaldo Santos <jesantosjr@protonmail.com>
2021-03-27 16:41:23 -0300

    style: setup global styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from '../styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Boilerplate</title>
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
