67a3f0aa18ccd9e73eb63c698961a7f681acf16c
Evaldo Santos <jesantosjr@protonmail.com>
2021-03-27 21:14:24 -0300

    test: update snapshots

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -10,29 +10,7 @@ describe('<Main />', () => {
       screen.getByRole('heading', { name: /react boilerplate/i })
     ).toBeInTheDocument()
 
-    expect(container).toMatchInlineSnapshot(`
-      <div>
-        <main
-          class="styles__Wrapper-sc-19t6oi2-0 fSRjTo"
-        >
-          <img
-            alt="React Boilerplate Logo"
-            class="styles__Logo-sc-19t6oi2-1 klGhdy"
-            src="/img/logo.svg"
-          />
-          <h1
-            class="styles__Title-sc-19t6oi2-2 lduiBl"
-          >
-            React Boilerplate
-          </h1>
-          <h2
-            class="styles__Description-sc-19t6oi2-3"
-          >
-            TypeScript, ReactJS, NextJS e Styled Components
-          </h2>
-        </main>
-      </div>
-    `)
+    expect(container).toMatchSnapshot()
   })
 
   it('should render the colors correctly', () => {
